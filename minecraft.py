1c7d99a6acab37a832941c08a059a55b4426c643
paulino-s <paulinovasquez900@gmail.com>
2022-02-11 18:31:41 -0600

    voxels

--- minecraft.py
+++ minecraft.py
@@ -1,7 +1,29 @@
-from crypt import crypt
-from pyexpat import model
-from statistics import mode
+
 from ursina import *
+
+class Test_cube(Entity):
+    def __init__(self):
+        super().__init__(
+            model = 'cube',
+            color = color.white,
+            texture = 'white_cube',
+            rotation = Vec3(45,45,45)
+        )
+
+class Test_button(Button):
+    def __init__(self):
+        super().__init__(
+            parent = scene,
+            model = 'cube',
+            texture = 'brick',
+            color = color.blue,
+            highlight_color = color.red,
+            pressed_color = color.lime
+        )
+    def input(self, key):
+        if self.hovered:
+            if key == 'left mouse down':
+                print('button pressed')
 
 def update():
     if held_keys['a']:
@@ -19,5 +41,7 @@ test_square = Entity(
 sans_texture = load_texture('assets/crypto.png')
 sans = Entity(model = 'quad',
 texture = sans_texture)
+
+tests_cube = Test_button()
 
 app.run()
